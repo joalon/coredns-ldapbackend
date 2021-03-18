84dae3dbebca1d7657fb940f2560cb0a7d697b5f
joalon <joakimlonnegren@gmail.com>
2021-03-18 07:35:28 +0100

    Remove test container when it stops

--- start-test-env.sh
+++ start-test-env.sh
@@ -2,8 +2,8 @@
 
 # Entire script taken from the osixia/phpldapadmin repo
 
-docker run --name ldap-service --hostname ldap-service --detach -P osixia/openldap:1.4.0
-docker run --name phpldapadmin-service --hostname phpldapadmin-service --link ldap-service:ldap-host --env PHPLDAPADMIN_LDAP_HOSTS=ldap-host --detach osixia/phpldapadmin:0.9.0
+docker run --rm --name ldap-service --hostname ldap-service --detach -P osixia/openldap:1.4.0
+docker run --rm --name phpldapadmin-service --hostname phpldapadmin-service --link ldap-service:ldap-host --env PHPLDAPADMIN_LDAP_HOSTS=ldap-host --detach osixia/phpldapadmin:0.9.0
 
 OPENLDAP_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" ldap-service)
 PHPLDAP_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" phpldapadmin-service)
